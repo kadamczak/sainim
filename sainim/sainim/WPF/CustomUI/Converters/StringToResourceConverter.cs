fea66f2a84e9d10249473b6b481641f9e66cdc48
kadamczak <cynder77@wp.pl>
2024-11-11 13:19:00 +0100

    Add string converter for unnamed layers

---
+++ sainim/sainim/WPF/CustomUI/Converters/StringToResourceConverter.cs
@@ -0,0 +1,20 @@
+﻿using sainim.Models.Extensions;
+using System.Windows.Data;
+
+namespace sainim.WPF.CustomUI.Converters
+{
+    /// <summary>
+    /// The <c>StringToResourceConverter</c> class converts string key into a string taken from the loaded resource file.
+    /// </summary>
+    public class StringToResourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string valueStr = value.ToString();
+            return (string.IsNullOrEmpty(valueStr)) ? "Unnamed".Resource() : valueStr;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
